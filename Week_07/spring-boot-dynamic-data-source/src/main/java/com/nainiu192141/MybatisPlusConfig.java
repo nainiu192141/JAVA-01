7abbd9f33c1df2bf2fffc0136f943d4e6e7edd76
xufengxiang <hzxufengxiang@highstore.cn>
2021-03-06 20:47:33 +0800

    修改

---
+++ Week_07/spring-boot-dynamic-data-source/src/main/java/com/nainiu192141/MybatisPlusConfig.java
@@ -0,0 +1,13 @@
+package com.nainiu192141;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by xfx on 2021/3/6 20:23
+ * @author 86134
+ */
+@Configuration
+@MapperScan("com.nainiu192141.mapper")
+public class MybatisPlusConfig {
+}
--- Week_07/spring-boot-dynamic-data-source/src/test/java/com/nainiu192141/SpringBootDynamicDataSourceApplicationTests.java
+++ Week_07/spring-boot-dynamic-data-source/src/test/java/com/nainiu192141/SpringBootDynamicDataSourceApplicationTests.java
@@ -1,6 +1,11 @@
 package com.nainiu192141;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.nainiu192141.entity.SysUser;
+import com.nainiu192141.mapper.SysUserMapper;
 import com.nainiu192141.service.SysUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -8,6 +13,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @SpringBootTest
 @Slf4j
@@ -17,6 +26,8 @@ public class SpringBootDynamicDataSourceApplicationTests {
     @Autowired
     private SysUserService userService;
 
+    @Autowired
+    private SysUserMapper sysUserMapper;
     @Test
     public void contextLoads() {
         SysUser user = userService.getById(1);
@@ -31,4 +42,50 @@ public class SpringBootDynamicDataSourceApplicationTests {
         log.info("第二个数据库 : [{}]", user2.toString());
     }
 
+    @Test
+    public void aInsert() {
+        SysUser user = new SysUser();
+        user.setUsername("zhangsan");
+        user.setUserId(20L);
+        sysUserMapper.insert(user);
+    }
+    @Test
+    public void bDelete() {
+        sysUserMapper.deleteById(1);
+        List<Integer> ids = new ArrayList<>();
+        ids.add(2);
+        ids.add(3);
+        sysUserMapper.deleteBatchIds(ids);
+        LambdaQueryWrapper<SysUser> queryWrapper = new QueryWrapper<SysUser>().lambda();
+        queryWrapper.eq(SysUser::getUserId,4);
+        sysUserMapper.delete(queryWrapper);
+        HashMap map = new HashMap();
+        map.put("user_id",5);
+        sysUserMapper.deleteByMap(map);
+    }
+    @Test
+    public void cUpdate() {
+        SysUser user = sysUserMapper.selectById(1);
+        user.setUserId(30L);
+        sysUserMapper.updateById(user);
+        LambdaUpdateWrapper<SysUser> updateWrapper = new UpdateWrapper<SysUser>().lambda();
+        updateWrapper.set(SysUser::getUserId,20).set(SysUser::getUsername,"zs");
+        sysUserMapper.update(user,updateWrapper);
+    }
+    @Test
+    public void dSelect() {
+        LambdaQueryWrapper<SysUser> queryWrapper = new QueryWrapper<SysUser>().lambda();
+        queryWrapper.eq(SysUser::getUserId,1);
+        SysUser user1 = sysUserMapper.selectOne(queryWrapper);
+        System.out.println(user1);
+        List<Integer> ids = new ArrayList<>();
+        ids.add(1);
+        ids.add(2);
+        List<SysUser> userList = sysUserMapper.selectBatchIds(ids);
+        System.out.println(userList);
+        LambdaQueryWrapper<SysUser> queryWrapper1 = new QueryWrapper<SysUser>().lambda();
+        queryWrapper1.gt(SysUser::getUserId,10);
+        List<SysUser> userList1 =sysUserMapper.selectList(queryWrapper1);
+        System.out.println(userList1);
+    }
 }
